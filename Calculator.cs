82b1387cfe88eb75658ae072acc5a0dd62bd6c12
gamekiller30 <85796998+gamekiller30@users.noreply.github.com>
2022-07-25 18:16:52 +0200

    Add files via upload

---
+++ Calculator.cs
@@ -0,0 +1,50 @@
+Console.Write("Enter value A:");
+int vala = int.Parse(Console.ReadLine());
+
+
+Console.Write("Enter Calculation Type:");
+string calc = (Console.ReadLine());
+
+Console.Write("Enter value B:");
+int valb = int.Parse(Console.ReadLine());
+
+
+int result;
+
+
+switch (calc)
+{
+
+    case "*":
+        result = vala * valb;
+        Console.WriteLine("The result is {0}.", result);
+        break;
+
+    case "/":
+        result = vala / valb;
+        Console.WriteLine("The result is {0}.", result);
+        break;
+
+    case "+":
+        result = vala + valb;
+        Console.WriteLine("The result is {0}.", result);
+        break;
+
+
+    case "-":
+        result = vala - valb;
+        Console.WriteLine("The result is {0}.", result);
+        break;
+
+    default:
+        Console.WriteLine("Wrong Value!");
+        break;
+
+}
+
+
+
+
+
+
+Console.ReadKey();
